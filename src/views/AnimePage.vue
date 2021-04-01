af9c054b7f213ce6072758b1ca63c6e9dc6a1ea6
ciobanuradu96 <ciobanu.radu96@gmail.com>
2021-04-01 17:50:36 +0300

    animePage update

--- src/views/AnimePage.vue
+++ src/views/AnimePage.vue
@@ -1,32 +1,41 @@
 <template>
-  <div class="flex w-screen h-screen">
-    <div class="flex flex-col my-auto items-center m-5">
-      <div class="flex flex-col">
-        <!-- <div class="w-screen">
-                <iframe :src="animeData.trailer_url+'&mute=1'" class=" w-2/4"></iframe>
-            </div> -->
+  <div class="flex w-screen h-screen ">
+    <div class="flex flex-col my-auto items-center p-5  bg-blue-500 ">
+      <div class="flex flex-col items-center ">
 
-        <div class="font-bold text-5xl text-blue-50 m-2">
+        <div class="w-60 h-60 rounded-3xl  bg-center bg-content border-2 shadow-md" :style="{backgroundImage:`url(${animeData.image_url})`}">
+        </div>
+        <div class="font-bold text-5xl text-blue-50 m-2 text-center">
           {{ animeData.title }}
         </div>
-        <div class="flex">
-          <div class="">
-            <img :src="animeData.image_url" alt="" class="" />
-          </div>
-          <div>
-            <p>Score {{ animeData.score }}</p>
-            <p>Popularity # {{ animeData.popularity }}</p>
-            {{ animeData.title_japanese }}
-            {{ animeData.title_synonyms }}
-            {{ animeData.status }}
+        <div class="space-x-4 text-center text-blue-100">
+            <div class="inline-block p-2 border-2 rounded-3xl shadow-md">Score: {{ animeData.score }}</div>
+            <div class="inline-block p-2 border-2 rounded-3xl shadow-md">Popularity: #{{ animeData.popularity }}</div>
+            <div class="inline-block p-2 border-2 rounded-3xl shadow-md">{{ animeData.status }}</div>
+        
+        <!-- {{ animeData.title_japanese }}
+        {{ animeData.title_synonyms }}
+        {{ animeData.status }}
+        {{ animeData.premiered }}
+        {{ animeData.type }} -->
 
-            {{ animeData.premiered }}
-            {{ animeData.type }}
-          </div>
         </div>
-        <div class="border-2 p-2 text-justify text-blue-100 border-blue-300">
-          {{ animeData.synopsis }}
+        
+        <div class="border-2 p-2 text-justify text-blue-100 border-blue-300 w-full my-4 shadow-md rounded-3xl">
+          <p class="p-2  overflow-clip overflow-hidden ..." :class="this.show ?'h-auto':'h-32'">  {{ animeData.synopsis }}</p>
+          <p class="text-right font-light text-sm text-blue-100 px-2 cursor-pointer" :class="this.show ? 'hidden':'' " @click="show=true">show more</p>
+          <p class="text-right font-light text-sm text-blue-100 px-2 cursor-pointer" :class="this.show ? '':'hidden' " @click="show=false">show less</p>
         </div>
+        <div class="w-full">
+            <iframe :src="animeData.trailer_url+'&mute=1'" class=" mx-auto"></iframe>
+        </div>
+        <div class="flex flex-col items-center"> 
+            <div>Characthers</div>
+            <div class="flex"> 
+                <div class="w-24 h-36 m-2 border-2 bg-center rounded-3xl bg-contain bg-no-repeat" v-for="(n,index) in 2" :key="index" :style="{backgroundImage:`url(${animeCharData[n].image_url})`}">
+            </div>        
+            </div>
+        </div>    
       </div>
     </div>
   </div>
@@ -39,6 +48,8 @@ export default {
     return {
       animeId: null,
       animeData: null,
+      animeCharData: null,
+      show:false
     };
   },
   created() {
@@ -49,6 +60,12 @@ export default {
       .then((response) => {
         this.animeData = response.data;
       });
+      axios
+      .get(`https://api.jikan.moe/v3/anime/${this.animeId}/characters_staff`)
+      .then((response) => {
+        this.animeCharData = response.data.characters;
+      });  
+
   },
 };
 </script>
